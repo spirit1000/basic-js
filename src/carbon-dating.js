b0b415969302f3e23084f3cf8c9cedd550b384a6
Victoria Vosipova <crank-line@rambler.ru>
2021-03-01 00:28:10 +0300

    feat: 1, 2 tasks complete

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+
+module.exports = function dateSample(sampleActivity) {
+  console.log(sampleActivity);
+  if (typeof sampleActivity != 'string') return false;
+  sampleActivity = +sampleActivity;
+
+  if (!sampleActivity) return false;
+  if (sampleActivity > 15 || sampleActivity <= 0) return false;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / 0.693 * HALF_LIFE_PERIOD);
   // remove line with error and write your code here
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(backyard) {
+  let catsNumber = 0;
+  for (let i = 0; i < backyard.length; i++) {
+    for (let j = 0; j < backyard[i].length; j++) {
+      if (backyard[i][j] === '^^')
+        catsNumber += 1;
+    }
+  }
+  return catsNumber;
   // remove line with error and write your code here
 };
