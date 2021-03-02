49e79836499075fa6d3e01feec02a002b41a8802
Victoria Vosipova <crank-line@rambler.ru>
2021-03-02 22:09:32 +0300

    feat: 1, 2, 3, 5 solved

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  function isMembersArray() {
+    if (typeof members !== 'object') return false;
+  }
+
+  if (!members) return false;
+
+  let teamMembersNames = [];
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] !== 'string') continue;
+    teamMembersNames.push(members[i]);
+  }
+
+  for (let i = 0; i < teamMembersNames.length; i++) {
+    teamMembersNames[i] = teamMembersNames[i].trim().toUpperCase();
+
+  }
+
+  teamMembersNames.sort();
+
+  let teamName = [];
+
+  for (let i = 0; i < teamMembersNames.length; i++) {
+    teamName.push(teamMembersNames[i].substring(0, 1));
+  }
+
+  teamName = teamName.join('');
+
+
+  return teamName;
 };
