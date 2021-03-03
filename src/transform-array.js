2e2ab31a54b23f19665150bad629d58149bc5250
Victoria Vosipova <crank-line@rambler.ru>
2021-03-03 21:50:29 +0300

    feat: partly solved 3 , 5 tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  if (typeof arr !== "object") throw Error;
+
+  if (!arr) return arr;
+
+  let copyArr = [];
+
+  let transformArr = [];
+
+  for (let i = 0; i < copyArr.length; i++) {
+
+    if (copyArr[i] === '--discard-next' && copyArr[i + 1]) copyArr[i + 1] = undefined;
+
+    if (copyArr[i] === '--discard-prev' && copyArr[i - 1]) copyArr[i - 1] = undefined;
+
+    if (copyArr[i] === '--double-next' && copyArr[i + 1]) copyArr.slice(i + 1, 0, copyArr[i + 1]);
+
+    if (copyArr[i] === '--double-prev' && copyArr[i - 1]) copyArr.slice(i - 1, 0, copyArr[i - 1]);
+
+  }
+
+  for (let i = 0; i < copyArr.length; i++) {
+    if (typeof copyArr[i] === 'undefined' ||
+      copyArr[i] === '--discard-next' ||
+      copyArr[i] === '--discard-prev' ||
+      copyArr[i] === '--double-next' ||
+      copyArr[i] === '--double-prev'
+    ) continue;
+    transformArr.push(copyArr[i]);
+  }
+
+
+  return transformArr;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  console.log(date);
+  if (!date) return 'Unable to determine the time of year!';
+  if (typeof date !== 'object') throw Error;
+  if (typeof date == 'function') throw Error;
+
+  let month = date.getMonth();
+  let day = date.getDate();
+  let year = date.getFullYear();
+
+  if (year < 0) throw Error;
+
+  function isLeapYear() {
+    if (year < 100 && year % 4 === 0) return true;
+    if (year < 400 && year % 4 === 0 && year % 100 !== 0) return true;
+    if (year % 400 === 0) return true;
+  }
+
+  if (!isLeapYear() && day === 28) throw Error;
+
+
+  if ((month == 0 && day >= 0 && day < 31) ||
+    (month == 1 && day >= 0 && day < 29) ||
+    (month == 11 && day >= 0 && day < 31)) {
+    return 'winter';
+  } else if ((month == 2 && day >= 0 && day < 31) ||
+    (month == 3 && day >= 0 && day < 30) ||
+    (month == 4 && day >= 0 && day < 31)) {
+    return 'spring';
+  } else if ((month == 5 && day >= 0 && day < 30) ||
+    (month == 6 && day >= 0 && day < 31) ||
+    (month == 7 && day >= 0 && day < 31)) {
+    return 'summer';
+  } else if ((month == 8 && day >= 0 && day < 30) ||
+    (month == 9 && day >= 0 && day < 31) ||
+    (month == 10 && day >= 0 && day < 30)) {
+    return 'autumn';
+  } else throw Error;
+
 };
