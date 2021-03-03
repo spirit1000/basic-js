c7d891ba8093f52d4a19c78e89723447bf9cf55c
Victoria Vosipova <crank-line@rambler.ru>
2021-03-03 22:55:18 +0300

    feat: 5 task solved

--- src/transform-array.js
+++ src/transform-array.js
@@ -4,21 +4,25 @@ module.exports = function transform(arr) {
 
   if (typeof arr !== "object") throw Error;
 
-  if (!arr) return arr;
-
-  let copyArr = [];
+  let copyArr = arr.slice();
 
   let transformArr = [];
 
   for (let i = 0; i < copyArr.length; i++) {
 
-    if (copyArr[i] === '--discard-next' && copyArr[i + 1]) copyArr[i + 1] = undefined;
+    if (copyArr[i] === '--discard-next' && (typeof copyArr[i + 1] !== 'undefined')) {
+      copyArr[i + 1] = undefined;
+      i += 1;
+    }
 
-    if (copyArr[i] === '--discard-prev' && copyArr[i - 1]) copyArr[i - 1] = undefined;
+    if (copyArr[i] === '--discard-prev' && (typeof copyArr[i - 1] !== 'undefined')) copyArr[i - 1] = undefined;
 
-    if (copyArr[i] === '--double-next' && copyArr[i + 1]) copyArr.slice(i + 1, 0, copyArr[i + 1]);
+    if (copyArr[i] === '--double-next' && (typeof copyArr[i + 1] !== 'undefined')) copyArr.splice(i + 1, 0, copyArr[i + 1]);
 
-    if (copyArr[i] === '--double-prev' && copyArr[i - 1]) copyArr.slice(i - 1, 0, copyArr[i - 1]);
+    if (copyArr[i] === '--double-prev' && (typeof copyArr[i - 1] !== 'undefined')) {
+      copyArr.splice(i - 1, 0, copyArr[i - 1]);
+      i += 1;
+    }
 
   }
 
