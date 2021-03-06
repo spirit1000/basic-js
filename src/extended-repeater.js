41df2ddcee5369373d23d5000a9e2a5807030184
Victoria Vosipova <crank-line@rambler.ru>
2021-03-06 19:18:16 +0300

    feat: add solver repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+  console.log(str);
+  console.log(options);
+
+  if (options.hasOwnProperty('addition')) {
+    options.addition = String(options.addition);
+  } else {
+    options.addition = "";
+  }
+
+  str = String(str);
+
+  if (!(options.hasOwnProperty('separator'))) options.separator = "+";
+  if (!(options.hasOwnProperty('additionSeparator'))) options.additionSeparator = "|";
+  if (!(options.hasOwnProperty('additionRepeatTimes'))) options.additionRepeatTimes = 1;
+
+
+  let additionString = options.addition;
+
+  for (let i = 2; i <= options.additionRepeatTimes; i++) {
+    additionString = additionString + options.additionSeparator + options.addition;
+  }
+
+  let finishString = str + additionString;
+
+  for (let i = 1; i < options.repeatTimes; i++) {
+    finishString = finishString + options.separator + str + additionString;
+  }
+
+  return finishString;
+
+
 };
-  
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+
+    let newArr = arr.slice();
+    let counter = 1;
+
+    for (let i = 0; i < newArr.length; i++) {
+      if (Array.isArray(newArr[i])) {
+        counter += 1;
+        newArr = newArr[i].slice();
+        i = 0;
+      }
+    }
+
+    return counter;
+
+
   }
 };
