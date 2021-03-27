14e6b271ecadd957bfab838387fdd4a038734d33
Vitoria Vosipova <crank-line@rambler.ru>
2021-03-28 00:53:44 +0300

    feat: add simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -3,22 +3,44 @@ const CustomError = require("../extensions/custom-error");
 const chainMaker = {
 
   array: [],
+  finishArray: [],
 
   getLength() {
-    return this.array.length;
+    this.array.length;
+    
   },
-  addLink(value) {
-    this.array.push(value);
+  addLink(value = '') {
+    
+      value = `( ${value} )`;
+      this.array.push(value);
+    
+    
+    return this;
+    
   },
   removeLink(position) {
-    this.array.splice(position, 1);
+    if (position <= this.array.length && position == Math.round(position) && position >= 0) {
+    this.array.splice(position-1, 1);
+    return this;
+    } else {
+      
+      this.array.length = 0;
+      throw new Error();
+    }
   },
   reverseChain() {
     this.array.reverse();
+    return this;
   },
   finishChain() {
-    return this.array.join('~~');
-  }
+    this.finishArray = this.array.join('~~');
+    this.array = [];
+    return this.finishArray;
+  },
+
+  
 };
+
+
 
 module.exports = chainMaker;
